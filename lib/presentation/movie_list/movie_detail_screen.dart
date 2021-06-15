f799873169e85e999a061067f18bf605ee8ddb3c
Harshada <harshada.punjabi@gmail.com>
2021-06-15 11:15:18 +0530

    changes to the movie details view

--- lib/presentation/movie_list/movie_detail_screen.dart
+++ lib/presentation/movie_list/movie_detail_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:newfluttermovieapp/domain/usecase/get_movie_trailer_usecase.dart';
 import 'package:newfluttermovieapp/presentation/base/view/movie_landing_base_view.dart';
+import 'package:newfluttermovieapp/presentation/movie_list/widgets/icon_label_circular_border.dart';
+import 'package:newfluttermovieapp/presentation/movie_list/widgets/movie_detail_header.dart';
 import 'package:provider/provider.dart';
-
 import 'package:newfluttermovieapp/presentation/model/movie_item.dart';
-import 'package:newfluttermovieapp/presentation/movie_list/widgets/movie_detail_app_bar.dart';
-import 'package:newfluttermovieapp/presentation/movie_list/widgets/movie_detail_description.dart';
 import 'movie_detail_viewmodel.dart';
 import 'widgets/trailer_layout_widget.dart';
 
@@ -54,28 +53,113 @@ class _MovieDetailScreenState extends MovieViewBaseState<MovieDetailViewModel, M
           return CustomScrollView(
             controller: _scrollController,
             slivers: [
-              MovieDetailAppBar(
-                movieList: widget.movieItem,
-                isShrink: isShrink,
+              SliverPersistentHeader(
+                pinned: true,
+                delegate: MoviePageSliverHeaderDelegate(movie: widget.movieItem),
               ),
-              SliverPadding(
-                padding: const EdgeInsets.all(8.0),
-                sliver: SliverFillViewport(
-                  delegate: SliverChildBuilderDelegate(
-                        (BuildContext context, int index) {
-                      return Container(
+              SliverToBoxAdapter(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Opacity(
+                        opacity: 1,
                         child: Padding(
-                          padding: EdgeInsets.all(5.0),
+                          padding: const EdgeInsets.all(24),
                           child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            crossAxisAlignment:
-                            CrossAxisAlignment.start,
-                            children: [
-                              MovieDetailDescription(
-                                movieList: widget.movieItem,
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              SizedBox(height: 12),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  IconLabelContainerBorderCircular(
+                                    iconData: Icons.language,
+                                    title: 'English',
+                                    color: Colors.white.withOpacity(0.75),
+                                  ),
+                                  IconLabelContainerBorderCircular(
+                                    iconData: Icons.favorite,
+                                    title: widget.movieItem.vote.toString(),
+                                    color: Colors.red.withOpacity(0.75),
+                                  ),
+                                  IconLabelContainerBorderCircular(
+                                    iconData: Icons.star,
+                                    title: widget.movieItem.vote.toString(),
+                                    color: Colors.amber.withOpacity(0.75),
+                                  ),
+                                  IconLabelContainerBorderCircular(
+                                    iconData: Icons.remove_red_eye,
+                                    title: widget.movieItem.vote.toString(),
+                                    color: Colors.white.withOpacity(0.75),
+                                  ),
+                                  IconLabelContainerBorderCircular(
+                                    iconData: Icons.remove_red_eye,
+                                    title: widget.movieItem.vote.toString(),
+                                    color: Colors.white.withOpacity(0.75),
+                                  ),
+                                ],
                               ),
-                              SizedBox(
-                                height: 10.0,
+                              SizedBox(height: 40),
+                              Text(
+                                'Synopsis',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  letterSpacing: 1.05,
+                                  color: Colors.white.withOpacity(0.8),
+                                ),
+                              ),
+                              Container(
+                                width: 35,
+                                height: 2,
+                                color: Colors.white.withOpacity(0.25),
+                                margin: const EdgeInsets.symmetric(vertical: 12),
+                              ),
+                              Text(
+                                widget.movieItem.overview,
+                                textAlign: TextAlign.justify,
+                                maxLines: 20,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  height: 1.5,
+                                  color: Colors.white.withOpacity(0.65),
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.white.withOpacity(0.05),
+                                    // border: Border.all(
+                                    //   color: Colors.white.withOpacity(0.35),
+                                    //   width: 0.5,
+                                    // ),
+                                    borderRadius: BorderRadius.circular(4)),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 12, vertical: 8),
+                                margin: const EdgeInsets.only(bottom: 18, top: 22),
+                                child: Text(
+                                  'Read More',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white.withOpacity(0.7),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 32, bottom: 18, left: 8, right: 16),
+                                child: Text(
+                                  'Trailer',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white.withOpacity(0.65),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
                               ),
                               Flexible(
                                 child: TrailerLayout(
@@ -87,9 +171,8 @@ class _MovieDetailScreenState extends MovieViewBaseState<MovieDetailViewModel, M
                             ],
                           ),
                         ),
-                      );
-                    },
-                    childCount: 1,
+                      ),
+                    ],
                   ),
                 ),
               ),
